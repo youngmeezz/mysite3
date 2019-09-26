3d926dfb30f0efcbabc122c5b7c97ab7c9e0740d
youngmeezz <kymee0918@gmail.com>
2019-09-26 15:59:39 +0900

    guestbook.xml 쿼리 수정중(delete문 패스워드 일치여부 확인 필요)

--- src/main/java/kr/co/itcen/mysite/repository/GuestBookDao.java
+++ src/main/java/kr/co/itcen/mysite/repository/GuestBookDao.java
@@ -13,109 +13,40 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kr.co.itcen.mysite.exception.GuestBookDaoException;
+import kr.co.itcen.mysite.exception.UserDaoException;
 import kr.co.itcen.mysite.vo.GuestBookVo;
+import kr.co.itcen.mysite.vo.UserVo;
 
 @Repository
 public class GuestBookDao {
 	
 	@Autowired
 	private SqlSession sqlSession;
-	
-	@Autowired 
-	private DataSource dataSource;
-	
-	public Boolean insert(GuestBookVo vo) {
-		Boolean result = false;
-		Connection connection = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		PreparedStatement pstmt = null;
 
-		try {
-			connection = dataSource.getConnection();
-
-			String sql = "insert into guestbook values(null,?,?,?, now())";
-			pstmt = connection.prepareStatement(sql);
-			pstmt.setString(1, vo.getText());
-			pstmt.setString(2, vo.getWriter());
-			pstmt.setString(3, vo.getPassword());
-
-			int count = pstmt.executeUpdate();
-			result = (count == 1);
-
-			stmt = connection.createStatement();
-
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (stmt != null) {
-					stmt.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
-	}
-
-
-	
-	public void delete(GuestBookVo vo) {
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+	//방명록 글쓸때 insert
+	public Boolean insert(GuestBookVo vo) throws GuestBookDaoException {
 		
-		try {
-			connection = dataSource.getConnection();
-			
-			String sql = "select password from guestbook where no = ?";
-			pstmt = connection.prepareStatement(sql);
-			pstmt.setLong(1, vo.getNo());
-			rs = pstmt.executeQuery();
-			
-			String pass = null;
-			
-			if(rs.next()) {
-				pass = rs.getString(1);
-			}
-			
-			if(pass.equals(vo.getPassword())) {
-				sql = "delete from guestbook where no = ?";
-				pstmt = connection.prepareStatement(sql);
-				pstmt.setLong(1, vo.getNo());
-				
-				pstmt.executeUpdate();
-			}
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		int count = sqlSession.insert("guestbook.insert", vo);
+		return count == 1;
+	}
+		
+
+//		//번호로 검색해서 가져올때의 get 오버로드1
+//		public UserVo get(Long no) {
+//			return sqlSession.selectOne("user.getByNo", no);
+//		}
+//		
+
+	//방명록 글 삭제할 때 delete
+	public Boolean delete(GuestBookVo vo) throws GuestBookDaoException{
+		
+		int count = sqlSession.delete("guestbook.delete", vo);
+		return count == 1;
 	}
 	
+	//방명록 글 조회할 때 getList
 	public List<GuestBookVo> getList() {
-//		List<GuestBookVo> result = new ArrayList<GuestBookVo>();
 		List<GuestBookVo> result = sqlSession.selectList("guestbook.getList");
 		return result;
 	}
